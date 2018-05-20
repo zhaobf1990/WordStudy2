5226b8a281c316864a8a769f35fdf8203a25d567
zbf <zbf@timevale.com>
2018-05-20 12:26:29 +0800

    初始化项目

---
+++ src/main/java/com/example/zhaobf/wordstudy/WordStudyApplication.java
@@ -0,0 +1,12 @@
+package com.example.zhaobf.wordstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordStudyApplication.class, args);
+	}
+}
